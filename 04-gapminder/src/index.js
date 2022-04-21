188b60f036bcaa5470d49d23f7e53f4d05ba6036
David Wismer <david.wismer@heig-vd.ch>
2022-04-21 12:40:00 +0200

    graph animé de l exercice 4

--- 04-gapminder/src/index.js
+++ 04-gapminder/src/index.js
@@ -1,4 +1,5 @@
 import * as d3 from 'd3'
+import { interval } from 'd3'
 
 // Pour importer les données
 import gdp from '../data/income_per_person_gdppercapita_ppp_inflation_adjusted.csv'
@@ -35,7 +36,6 @@ gdp.forEach(pays => {
     }
     pays['2021'] = gdpAnneeCourante;
 
-    // Générer une taille d'axe X cohérente
     if (pays['2021'] >= theBiggestGDP) {
         theBiggestGDP = pays['2021'];
     }
@@ -156,7 +156,6 @@ d3.json("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/w
         .attr("fill", function (d) {
             let color = ''
             lifeExpectancy.forEach(pays => {
-                if (d.properties.name == 'USA') console.log(d.properties.name)
                 if (pays['country'] == d.properties.name) {
                     color = colorScaleChoro(pays['2021'])
                 }
@@ -184,3 +183,192 @@ function randomScheme() {
 }
 
 /////////////////////////////////////////EXERCICE 3
+
+// CODE DONNÉ POUR L'EXERCICE :
+
+// Récupère toutes les années
+const annees = Object.keys(population[0])
+
+let pop = [],
+    income = [],
+    life = [],
+    dataCombined = [];
+
+// Merge data
+const mergeByCountry = (a1, a2, a3) => {
+    let data = [];
+    a1.map(itm => {
+        let newObject = {
+            ...a2.find((item) => (item.country === itm.country) && item),
+            ...a3.find((item) => (item.country === itm.country) && item),
+            ...itm
+        }
+        data.push(newObject);
+    })
+    return data;
+}
+
+annees.forEach(annee => {
+    pop.push({ "annee": annee, "data": converterSI(population, annee, "pop") })
+    income.push({ "annee": annee, "data": converterSI(gdp, annee, "income") })
+    life.push({ "annee": annee, "data": converterSI(lifeExpectancy, annee, "life") })
+    const popAnnee = pop.filter(d => d.annee == annee).map(d => d.data)[0];
+    const incomeAnnee = income.filter(d => d.annee == annee).map(d => d.data)[0];
+    const lifeAnnee = life.filter(d => d.annee == annee).map(d => d.data)[0];
+    dataCombined.push({ "annee": annee, "data": mergeByCountry(popAnnee, incomeAnnee, lifeAnnee) })
+});
+
+function converterSI(array, variable, variableName) {
+    let convertedVariable = array.map(d => {
+        // Trouver le format SI (M, B, k)
+        let SI = typeof d[variable.toString()] === 'string' || d[variable.toString()] instanceof String ? d[variable.toString()].slice(-1) : d[variable.toString()];
+        // Extraire la partie numérique
+        let number = typeof d[variable.toString()] === 'string' || d[variable.toString()] instanceof String ? parseFloat(d[variable.toString()].slice(0, -1)) : d[variable.toString()];
+        // Selon la valeur SI, multiplier par la puissance
+        switch (SI) {
+            case 'M': {
+                return { "country": d.country, [variableName]: Math.pow(10, 6) * number };
+            }
+            case 'B': {
+                return { "country": d.country, [variableName]: Math.pow(10, 9) * number };
+            }
+            case 'k': {
+                return { "country": d.country, [variableName]: Math.pow(10, 3) * number };
+            }
+            default: {
+                return { "country": d.country, [variableName]: number };
+            }
+        }
+    })
+    return convertedVariable;
+};
+
+//TRAVAIL PERSONNEL
+
+d3.select('body').append('h3').text('Graphique Animé')
+
+d3.select("body")
+    .append("div")
+    .attr('id', 'graphA')
+
+let marginA = { top: 10, right: 20, bottom: 30, left: 50 },
+    widthA = 1000 - marginA.left - marginA.right,
+    heightA = 600 - marginA.top - marginA.bottom;
+
+let svgA = d3.select("#graphA")
+    .append("svg")
+    .attr("width", widthA + marginA.left + marginA.right)
+    .attr("height", heightA + marginA.top + marginA.bottom)
+    .append("g")
+    .attr("transform", "translate(" + marginA.left + "," + marginA.top + ")")
+
+// Générer une taille d'axe X cohérente
+let theBiggestGDPA = 0;
+dataCombined.forEach(annee => {
+    if (annee.annee <= '2022') {
+        annee.data.forEach(pays => {
+            if (pays.income >= theBiggestGDPA) theBiggestGDPA = pays.income
+        })
+    }
+})
+
+//Ajouter X
+let xA = d3.scaleSqrt()
+    .domain([0, theBiggestGDPA])
+    .range([0, widthA]);
+svgA.append("g")
+    .attr("transform", "translate(0," + heightA + ")")
+    .call(d3.axisBottom(xA));
+
+// Générer une taille pour Y
+let theBiggestLifeExpA = 0;
+dataCombined.forEach(annee => {
+    annee.data.forEach(pays => {
+        if (pays.life >= theBiggestLifeExpA) {
+            theBiggestLifeExpA = pays.life
+        }
+    })
+})
+
+//Ajouter Y
+let yA = d3.scalePow()
+    .exponent(1.25)
+    .domain([0, theBiggestLifeExpA * 1.1])
+    .range([heightA, 0]);
+svgA.append('g')
+    .call(d3.axisLeft(yA))
+
+// Add a scale for bubble size
+let zA = d3.scaleLinear()
+    .domain([200000, 1310000000])
+    .range([5, 60]);
+
+//Créer le bouton de lancement d'animation
+let buttonA = d3.select('#graphA')
+    .append('svg')
+    .attr("width", widthA + marginA.left + marginA.right)
+    .attr("height", 100)
+    .attr('class', 'button')
+    .append('g')
+    .append('circle')
+    .attr('cx', '50')
+    .attr('cy', '50')
+    .attr('r', '40')
+    .attr('fill', 'grey')
+
+d3.select('.button').append('text').attr('class', 'afficheAnnee').text('1800').attr('transform', 'translate(100,62.5)')
+    .attr('font-size', 40).attr('font-family', 'helvetica')
+
+let clicked = false
+buttonA.on('click', () => {
+    if (clicked) {
+        stop()
+        clicked = false
+    } else {
+        animate()
+        clicked = true
+    }
+})
+
+//Gestion de l'affichage du texte
+let indexAnnee = -1
+function play() {
+    if (indexAnnee >= 222) {
+        indexAnnee = 0
+    } else {
+        indexAnnee++
+    }
+
+    let anneePause = dataCombined[indexAnnee].annee
+    d3.select('.afficheAnnee').text(anneePause)
+    updateData(dataCombined[indexAnnee])
+}
+
+function stop() {
+    clearInterval(intervalId)
+    intervalId = null
+}
+
+//Gestion intervalle d'animation
+let intervalId
+function animate() {
+    if (!intervalId) intervalId = setInterval(play, 50)
+}
+
+// Mise à jour des infos
+function updateData(annee) {
+    svgA.selectAll('circle')
+        .data(annee.data)
+        .join(enter => enter.append('circle')
+            .style('fill', `#${Math.floor(Math.random() * 16777215).toString(16)}`)
+            .attr('stroke', 'black')
+            .style("opacity", "0.7")
+            .attr('cx', function (d) { return xA(d.income) })
+            .attr('cy', function (d) { return yA(d.life) })
+            .attr('r', function (d) { return zA(d.pop) }),
+            update => update.attr('cx', function (d) { return xA(d.income) })
+                .attr('cy', function (d) { return yA(d.life) })
+                .attr('r', function (d) { return zA(d.pop) }),
+            exit => exit.remove()
+        )
+}
